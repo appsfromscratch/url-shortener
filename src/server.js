662a682ab9e80016e6f7440a80d4c611ca603333
Alex Leasenco <rubydeve@gmail.com>
2019-02-11 18:41:50 +0200

    Add validation for URL
    Add connection to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const { MongoClient } = require('mongodb');
+
+const databaseUrl = process.env.DATABASE;
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+MongoClient.connect(databaseUrl, { useNewUrlParser: true })
+  .then(client => {
+    app.locals.db = client.db('shortener');
+  })
+  .catch(() => console.error('Failed to connect to the database'));
 
 app.get('/', (req, res) => {
   const htmlPath = path.join(__dirname, 'public', 'index.html');
@@ -12,4 +27,19 @@ app.get('/', (req, res) => {
 app.set('port', process.env.PORT || 4100);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
+});
+
+app.post('/new', (req, res) => {
+  let originalUrl;
+  try {
+    originalUrl = new URL(req.body.url);
+  } catch (err) {
+    return res.status(400).send({ error: 'invalid URL' });
+  }
+
+  dns.lookup(originalUrl.hostname, (err) => {
+    if (err) {
+      return res.status(404).send({ error: 'Address not found' });
+    };
+  });
 });
